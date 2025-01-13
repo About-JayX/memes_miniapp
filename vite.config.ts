969d2f39161bd798d5da1c89d92eb449bd71e5be
Mr.Monkey <slerf@memes.ac>
2025-01-14 03:43:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 const env = process.argv[process.argv.indexOf('--mode') + 1].split('-')[1]
 export default defineConfig({
   base: '/',
+  assetsInclude: ['**/*.tgs', '**/*.json'],
   plugins: [
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), "./src/assets/icon")],
@@ -152,5 +153,4 @@ export default defineConfig({
       strict: true,
     },
   },
-  assetsInclude: ["**/*.tgs"], // 确保 Vite 包含这些文件
 });
